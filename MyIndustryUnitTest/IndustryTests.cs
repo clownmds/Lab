b2e4000f5f0cfa13db7d13d15e98f119bc4c268e
clownmds <45055487+clownmds@users.noreply.github.com>
2019-01-09 12:54:06 +0700

    Add files via upload

---
+++ MyIndustryUnitTest/IndustryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyIndustry.Model;
+
+namespace MyIndustryUnitTest
+{
+    [TestClass]
+    public class IndustryTests
+    {
+        [TestMethod]
+        public void IndustryConstructorTest()
+        {
+            string s="Завод";
+            Industry i = new Industry(s);
+        }
+
+        [TestMethod]
+        public void IndustryConstructorTest_Null()
+        {
+            string s = null;
+            Industry i = new Industry(s);
+        }
+
+        [TestMethod]
+        public void AddItemMetalWorkingTest()
+        {
+            string s = "Завод";
+            Industry i = new Industry(s);
+            i.AddItemMetalWorking(5000);
+        }
+
+        [TestMethod]
+        public void AddItemWoodWorkingTest()
+        {
+            string s = "Завод";
+            Industry i = new Industry(s);
+            i.AddItemWoodWorking(5000);
+        }
+    }
+}
---
+++ MyIndustryUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyIndustryUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyIndustryUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de1947b9-63fc-4386-b384-78aa00383736")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
