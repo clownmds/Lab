5b459ea2b59b6ed8feb7b376d637705c2b056925
clownmds <45055487+clownmds@users.noreply.github.com>
2019-01-24 21:54:03 +0700

    Add files via upload

---
+++ Industry.cs
@@ -0,0 +1,107 @@
+﻿using MyIndustry.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+using MyIndustry;
+
+namespace MyIndustry.Model
+{
+    public sealed class Industry : IIndustry
+    {
+        public string Title { get; private set; }
+
+        int countMetall = 1;
+        int countWood = 1;
+        public bool isRight=false;
+
+        public Industry(string title)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            isRight = true;
+        }
+
+        public ICollection<IPlant> Plants { get; private set; } = new List<IPlant>();
+
+        public void AddItemMetalWorking(int power)
+        {
+            if (power < 0)
+                throw new ArgumentOutOfRangeException();
+            if (power != 0)
+            {
+                var plant = new MetalWorking(countMetall, power);
+                Plants.Add(plant);
+                countMetall++;
+            }
+            else throw new ArgumentNullException();
+        }
+
+        public void AddItemWoodWorking(int power)
+        {
+            var plant = new WoodWorking(countWood, power);
+            Plants.Add(plant);
+            countWood++;
+        }
+
+        public void RemoveItem(string title)
+        {
+            var plant = SearchItem(title);
+            Plants.Remove(plant);
+        }
+
+        public List<IPlant> ViewItems()
+        {
+            return (List<IPlant>)Plants;
+        }
+
+        public void ChangeItem(string title, int power)
+        {
+            var plant = SearchItem(title);
+            plant.Power = power;
+        }
+
+        public IPlant SearchItem(string title)
+        {
+            var plant = from p in Plants
+                        where p.Title == title
+                        select p;
+            return plant.First();
+        }
+
+        public void Serialize()
+        {
+            var industryDTO = new IndustryDTO();
+            industryDTO.Title = Title;
+            foreach (Plant p in Plants)                    
+                industryDTO.Plants.Add(p);
+            XmlSerializer formatter = new XmlSerializer(typeof(IndustryDTO));
+            using (FileStream fs = new FileStream("industryDTO.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, industryDTO);
+            }
+        }
+
+        public void Deserialize()
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(IndustryDTO));
+            using (FileStream fs = new FileStream("industryDTO.xml", FileMode.OpenOrCreate))
+            {
+                var industryDTO = (IndustryDTO)formatter.Deserialize(fs);
+                Title = industryDTO.Title;
+                Plants.Clear();
+                foreach (IPlant p in industryDTO.Plants)
+                    Plants.Add(p);
+            }
+        }
+
+       /* public void Deserialize(IndustryDTO industryDTO)
+        {
+            Title = industryDTO.Title;
+            Plants.Clear();
+            foreach (IPlant p in industryDTO.Plants)
+                Plants.Add(p);
+            ViewItems();
+        }*/
+    }
+}
--- MyIndustryUnitTest/IndustryTests.cs
+++ MyIndustryUnitTest/IndustryTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyIndustry.Model;
+using MyIndustry.Interfaces;
+using System.Collections.Generic;
 
 namespace MyIndustryUnitTest
 {
@@ -11,14 +13,22 @@ namespace MyIndustryUnitTest
         public void IndustryConstructorTest()
         {
             string s="Завод";
+
             Industry i = new Industry(s);
+
+            Assert.IsTrue(i.isRight);
+            Assert.IsInstanceOfType(i, typeof(Industry));
+            Assert.AreEqual(i.Title, s);
         }
 
         [TestMethod]
-        public void IndustryConstructorTest_Null()
+        public void IndustryIncorrectConstructorTest_Null()
         {
             string s = null;
+
             Industry i = new Industry(s);
+            
+            Assert.IsFalse(i.isRight);
         }
 
         [TestMethod]
@@ -26,7 +36,15 @@ namespace MyIndustryUnitTest
         {
             string s = "Завод";
             Industry i = new Industry(s);
-            i.AddItemMetalWorking(5000);
+            int power = 5000;
+
+            i.AddItemMetalWorking(power);
+            
+            var ie=i.Plants.GetEnumerator();
+            ie.MoveNext();
+
+            Assert.IsInstanceOfType(ie.Current, typeof(IPlant));
+            Assert.AreEqual(ie.Current.Power, power);
         }
 
         [TestMethod]
@@ -34,7 +52,78 @@ namespace MyIndustryUnitTest
         {
             string s = "Завод";
             Industry i = new Industry(s);
+            int power = 5000;
+
+            i.AddItemWoodWorking(power);
+
+            var ie = i.Plants.GetEnumerator();
+            ie.MoveNext();
+
+            Assert.IsInstanceOfType(ie.Current, typeof(IPlant));
+            Assert.AreEqual(ie.Current.Power, power);
+        }
+
+        [TestMethod]
+        public void RemoveItemTest()
+        {
+            string s = "Завод";
+            Industry i = new Industry(s);
             i.AddItemWoodWorking(5000);
+            int power = 4000;
+            i.AddItemWoodWorking(power);
+            var ie = i.Plants.GetEnumerator();
+            ie.MoveNext();
+
+            i.RemoveItem(ie.Current.Title);
+
+            ie = i.Plants.GetEnumerator();
+            ie.MoveNext();
+            
+            Assert.AreEqual(ie.Current.Power, power);
+        }
+
+        [TestMethod]
+        public void ViewItemsTest()
+        {           
+            string s = "Завод";
+            Industry i = new Industry(s);
+            int power = 5000;
+            i.AddItemWoodWorking(power);
+
+            List<IPlant> p= i.ViewItems();
+
+            Assert.IsInstanceOfType(p, typeof(List<IPlant>));
+        }
+        
+        [TestMethod]
+        public void SearchItemTest()
+        {
+            string s = "Завод";
+            Industry i = new Industry(s);
+            int power = 5000;
+            i.AddItemWoodWorking(power);
+            var ie = i.Plants.GetEnumerator();
+            ie.MoveNext();
+
+            var p = i.SearchItem(ie.Current.Title);
+            
+            Assert.AreEqual(p.Title,ie.Current.Title);
+        }
+
+        [TestMethod]
+        public void ChangeItemTest()
+        {
+            string s = "Завод";
+            Industry i = new Industry(s);
+            int power = 5000;
+            i.AddItemWoodWorking(power);
+            var ie = i.Plants.GetEnumerator();
+            ie.MoveNext();
+            power = 1000;
+
+            i.ChangeItem(ie.Current.Title,power);
+            
+            Assert.AreEqual(ie.Current.Power,power);
         }
     }
 }
